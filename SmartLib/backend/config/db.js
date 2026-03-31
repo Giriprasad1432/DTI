273af86ef829202bb04924567798682a84135246
Giri prasad <giribabu121989@gmail.com>
2026-04-01 05:20:36 +0530

    fix: bypass 123 error with DATABASE_URL

--- SmartLib/backend/config/db.js
+++ SmartLib/backend/config/db.js
@@ -2,9 +2,9 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        const uri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/SmartLib';
+        const uri = process.env.DATABASE_URL || process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/SmartLib';
         const conn = await mongoose.connect(uri);
-        console.log(`mongoDB connected: ${conn.connection.host}`);
+        console.log(`✅ mongoDB connected: ${conn.connection.host}`);
     } catch (error) {
         console.log(error.message);
         process.exit(1);
